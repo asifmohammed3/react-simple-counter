d07cf346fbe2cb36816c4f7a28e99d7ab765656b
Mohammed Asif <82872316+asifmohammed3@users.noreply.github.com>
2022-12-01 23:53:17 +0530

    initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from "react";
+import  {useState} from 'react';
+import { Row, Col} from 'react-bootstrap'  
+function CounterPage() {
+let [countNum, setCount] = useState(0);
+let increaseValaue = () => {
+setCount((countNum += 1)); 
+};
+let decreaseValue = () => {
+if (countNum === 0) {
+alert("Let's stay positive, shall we?");
+} else {
+setCount((countNum -= 1)); 
 }
+};
+let resetValue = () => {
+setCount(0); 
+};
+return (  <div style={{
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',backgroundColor: "grey",height: '100vh',
 
-export default App;
+}} >
+  <Col>
+        <Row><button onClick={decreaseValue} style={{}}>-</button>
+</Row>
+        <Row><h1> {countNum} </h1>
+</Row>
+        <Row><button  onClick={increaseValaue}>+</button> 
+</Row>
+      </Col>
+
+  </div>
+
+
+
+
+)
+}
+export default CounterPage
